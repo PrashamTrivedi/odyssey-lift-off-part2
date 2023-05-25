69ef2799639aa6ec9889fa2bc0991d3e37dc10b3
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2023-05-25 14:47:36 +0000

    Fixed index so it will be loaded

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://prashamtrivedi-scaling-adventure-wrvxx7vjg9c9766-4000.preview.app.github.dev/chat',
   cache: new InMemoryCache(),
 });
 
